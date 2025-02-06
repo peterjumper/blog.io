b30382877c545b20088b8270c9235d6ddc35963b
peter <lplssjumper@gmail.com>
2025-02-06 00:41:43 -0500

    cccc

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/peterjumper/blog.io",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      // GitHub: "https://github.com/peterjumper/blog.io",
+      // "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
